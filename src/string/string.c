e81dbb142798be0e685fdd02c46019f439f02747
Alex Gustafsson <alex-gustafsson@live.se>
2019-09-12 11:05:45 +0200

    Implemented cursor functions, commented code

--- src/string/string.c
+++ src/string/string.c
@@ -15,13 +15,29 @@ string_t *string_create() {
   return string;
 }
 
-string_t *string_fromCopy(const char *buffer) {
+string_cursor_t *string_createCursor(string_t *string) {
+  string_cursor_t *stringCursor = malloc(sizeof(string_cursor_t));
+  if (stringCursor == 0)
+    return 0;
+
+  stringCursor->string = string;
+  stringCursor->offset = 0;
+
+  return stringCursor;
+}
+
+void string_resetCursor(string_cursor_t *stringCursor) {
+  stringCursor->offset = 0;
+}
+
+// NOTE: Currently trusts the length to be within the bounds of the buffer
+string_t *string_fromCopyWithLength(const char *buffer, size_t length) {
   string_t *string = string_create();
   if (string == 0)
     return 0;
 
-  string->size = strlen(buffer);
-  string->bufferSize = string->size + 1;
+  string->size = length;
+  string->bufferSize = length + 1;
   string->buffer = malloc(sizeof(char) * (string->bufferSize));
   if (string->buffer == 0) {
     free(string);
@@ -32,6 +48,12 @@ string_t *string_fromCopy(const char *buffer) {
   string->buffer[string->bufferSize - 1] = 0;
 
   return string;
+}
+
+string_t *string_fromCopy(const char *buffer) {
+  size_t length = strlen(buffer);
+
+  return string_fromCopyWithLength(buffer, length);
 }
 
 void string_append(string_t *string, const char *buffer) {
@@ -60,9 +82,78 @@ size_t string_getSize(string_t *string) {
   return string->size;
 }
 
+char string_getCharAt(string_t *string, size_t index) {
+  // Out of bounds
+  if (index >= string->size)
+    return 0;
+
+  char *characterAddress = (char *)(string->buffer + index);
+  // The characterAddress was not set (null)
+  if (characterAddress == 0)
+    return 0;
+
+  return (*characterAddress);
+}
+
+void string_setCharAt(string_t *string, size_t index, char character) {
+  // Out of bounds
+  if (index >= string->size)
+    return;
+
+  char *characterAddress = (char *)(string->buffer + index);
+  // The character was not set (null)
+  if (characterAddress == 0)
+    return;
+
+  (*characterAddress) = character;
+}
+
+string_t *string_substring(string_t *string, size_t firstIndex, size_t lastIndex) {
+  // Out of bounds
+  if (firstIndex >= string->size || lastIndex > string->size)
+    return 0;
+
+  string_t *substring = string_fromCopyWithLength(string->buffer + firstIndex, lastIndex - firstIndex);
+  // Unable to allocate substring
+  if (substring == 0)
+    return 0;
+
+  return substring;
+}
+
+char string_getNextChar(string_cursor_t *stringCursor) {
+  if (stringCursor->offset >= stringCursor->string->size)
+    return 0;
+
+  return string_getCharAt(stringCursor->string, stringCursor->offset++);
+}
+
+string_t *string_getNextLine(string_cursor_t *stringCursor) {
+  if (stringCursor->offset >= stringCursor->string->size)
+    return 0;
+
+  size_t stop;
+  for (stop = stringCursor->offset; stop < stringCursor->string->size; stop++) {
+    if (string_getCharAt(stringCursor->string, stop) == '\n')
+      break;
+  }
+
+  // Don't include the newline
+  string_t *line = string_substring(stringCursor->string, stringCursor->offset, stop);
+  // Move past the newline
+  stop++;
+  stringCursor->offset = stop;
+
+  return line;
+}
+
 void string_free(string_t *string) {
   if (string->buffer != 0)
     free(string->buffer);
 
   free(string);
+}
+
+void string_freeCursor(string_cursor_t *stringCursor) {
+  free(stringCursor);
 }
