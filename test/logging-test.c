f5e2b095445d53bc900ae279af516bb2e7df03b5
MarcusLenander <Marcuslenander96@gmail.com>
2019-10-04 09:59:51 +0200

    Updated unittest

--- test/logging-test.c
+++ test/logging-test.c
@@ -3,7 +3,7 @@
 #include "../src/logging/logging.h"
 
 void logging_test_canFormatTimeBasedOnSecondsAndNanoseconds() {
-  uint64_t rest = 0;
+  uint64_t nanoseconds = 0;
   uint64_t milliseconds = 0;
   uint64_t seconds = 0;
   uint64_t minutes = 0;
@@ -13,14 +13,14 @@ void logging_test_canFormatTimeBasedOnSecondsAndNanoseconds() {
   uint64_t secondsSinceStart = 258933;
   uint64_t nanosecondsSinceStart = 999440000;
 
-  time_formatTime(nanosecondsSinceStart, secondsSinceStart, &milliseconds, &seconds, &minutes, &hours, &days, &rest);
+  time_formatTime(nanosecondsSinceStart, secondsSinceStart, &nanoseconds, &milliseconds, &seconds, &minutes, &hours, &days);
 
   TEST_ASSERT_EQUAL_UINT64(2, days);
   TEST_ASSERT_EQUAL_UINT64(23, hours);
   TEST_ASSERT_EQUAL_UINT64(55, minutes);
   TEST_ASSERT_EQUAL_UINT64(33, seconds);
   TEST_ASSERT_EQUAL_UINT64(999, milliseconds);
-  TEST_ASSERT_EQUAL_UINT64(44, rest);
+  TEST_ASSERT_EQUAL_UINT64(44, nanoseconds);
 }
 
 void logging_test_run() {
